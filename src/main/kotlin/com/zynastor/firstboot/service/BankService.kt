9c613f5354dddbbd7b3957da61c032544d3fbc32
hhc <hh1183@gmail.com>
2023-06-14 21:51:29 +0900

    Tutorial 10 - PATCH

--- src/main/kotlin/com/zynastor/firstboot/service/BankService.kt
+++ src/main/kotlin/com/zynastor/firstboot/service/BankService.kt
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Service
 @Service
 class BankService(private val dataSource: BankDataSource) {
     fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+    fun getBank(accountNumber: String): Bank = dataSource.retrieveBank(accountNumber)
+    fun addBank(bank: Bank): Bank  = dataSource.createBank(bank)
+    fun updateBank(bank: Bank): Bank = dataSource.updateBank(bank)
 
 
-
-    fun getBank(accountNumber: String): Bank =dataSource.retrieveBank(accountNumber)
 }
